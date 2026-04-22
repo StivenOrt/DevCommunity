02b823dca011e5f57e75fc171af484099e50f74d
DANI <dannyandrestecnologia@outlook.com>
2026-04-22 17:43:52 -0500

    Implementacion de guard de rutas de roles y autoria

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -3,12 +3,12 @@ import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostEntity } from './entities/post.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { Getauthor } from '../auth/decorators/get-author.decorator';
 import { ApiTags, ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { Rols } from '../auth/decorators/rols.decorator';
 import { RolsGuard } from '../auth/guards/rols.guard';
-import { crearAutorGuard } from '../auth/guards/author.guard';
-const PostAutorGuard = crearAutorGuard(PostEntity);
+import { AutorGuard } from '../auth/guards/author.guard';
+import { Autor } from '../auth/decorators/authors.decorator';
+
+
 @UseGuards(JwtAuthGuard, RolsGuard)
 @ApiBearerAuth()
 @ApiTags('posts')
@@ -43,22 +43,22 @@ export class PostController {
   @ApiResponse({ status: 200, description: 'Devuelve el nuevo objeto de la publicacion' })
   @ApiResponse({ status: 404, description: 'Recurso no hallado con dicho id' })
   @ApiResponse({ status: 403, description: 'Se requiere un rol con el permiso requerido para actualizar' })
-  @UseGuards(PostAutorGuard)
-  @Rols('1')
+  @UseGuards(AutorGuard)
+  @Autor(PostEntity)
   @Patch(':id')
-  async updatePost( @Param('id', ParseIntPipe) id: number, @Body() updatePostDto: CreatePostDto, @Getauthor() author ): Promise<PostEntity> {
-    return this.postService.updatePost(id, updatePostDto, author);
+  async updatePost( @Param('id', ParseIntPipe) id: number, @Body() updatePostDto: CreatePostDto ): Promise<PostEntity> {
+    return this.postService.updatePost(id, updatePostDto);
   }
   
   @ApiOperation({ summary: 'Elimina una publicación' })
   @ApiResponse({ status: 200, description: 'Devuelve un mensaje de confirmación del borrado' })
   @ApiResponse({ status: 404, description: 'Publicación no hallada para eliminar' })
   @ApiResponse({ status: 403, description: 'Se requiere un rol con el permiso de realizar para eliminar' })
-  @UseGuards(PostAutorGuard)
-  @Rols('1', '2')
+  @UseGuards(AutorGuard)
+  @Autor(PostEntity)
   @Delete(':id')
-  async deletePost(@Param('id', ParseIntPipe) id: number, @Getauthor() author ): Promise<void> {
-    return this.postService.deletePost(id, author);
+  async deletePost(@Param('id', ParseIntPipe) id: number ): Promise<void> {
+    return this.postService.deletePost(id);
   }
 
 
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,12 +1,10 @@
-import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { PostEntity } from './entities/post.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
-import { RolsEnum } from 'src/common/enums/rols.enums';
 import { ensureExists } from 'src/common/utils/assertion.util';
 import { POST_ERRORS } from 'src/common/constants/error-messages';
-import { CrudEnums } from 'src/common/enums/crud.enums';
 
 @Injectable()
 export class PostService {
@@ -36,31 +34,24 @@ export class PostService {
 
     }
 
-    async updatePost(id: number, updatePostDto: CreatePostDto, author: any): Promise<PostEntity> {
+    async updatePost(id: number, updatePostDto: CreatePostDto): Promise<PostEntity> {
         const post = ensureExists(
             await this.getPostById(id),
             new NotFoundException( POST_ERRORS.NOT_FOUND() )
         )
-
-        if (post.authorId !== author.id) throw new ForbiddenException( POST_ERRORS.FORBIDDEN(CrudEnums.UPDATE) );
 
         Object.assign(post, updatePostDto);
 
         return this.postRepository.save(post);
     }
 
-    async deletePost(id: number, author: any): Promise<void> {
+    async deletePost(id: number): Promise<void> {
 
         const post = ensureExists(
             await this.getPostById(id),
             new NotFoundException(POST_ERRORS.NOT_FOUND())
         )
 
-        const isOwner = post.authorId === author.id;
-        const hasPrivileges = [RolsEnum.ADMIN, RolsEnum.MODERATOR].some(role => author.roles.include(role));
-
-        if (!isOwner && !hasPrivileges) throw new ForbiddenException( POST_ERRORS.FORBIDDEN(CrudEnums.DELETE) );
-        
         await this.postRepository.remove(post);
     }
 }
