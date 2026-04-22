4a64910ad47c5258799122acb27ec5958ea76c9f
Moncka20 <moncayothomas@gmail.com>
2026-04-22 15:12:08 -0500

    Refactor post controller and service to use Getauthor decorator for author retrieval

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -36,28 +36,28 @@ export class PostService {
 
     }
 
-    async updatePost(id: number, updatePostDto: CreatePostDto, user: any): Promise<PostEntity> {
+    async updatePost(id: number, updatePostDto: CreatePostDto, author: any): Promise<PostEntity> {
         const post = ensureExists(
             await this.getPostById(id),
             new NotFoundException( POST_ERRORS.NOT_FOUND() )
         )
 
-        if (post.authorId !== user.id) throw new ForbiddenException( POST_ERRORS.FORBIDDEN(CrudEnums.UPDATE) );
+        if (post.authorId !== author.id) throw new ForbiddenException( POST_ERRORS.FORBIDDEN(CrudEnums.UPDATE) );
 
         Object.assign(post, updatePostDto);
 
         return this.postRepository.save(post);
     }
 
-    async deletePost(id: number, user: any): Promise<void> {
+    async deletePost(id: number, author: any): Promise<void> {
 
         const post = ensureExists(
             await this.getPostById(id),
-            new NotFoundException()
+            new NotFoundException(POST_ERRORS.NOT_FOUND())
         )
 
-        const isOwner = post.authorId === user.id;
-        const hasPrivileges = [RolsEnum.ADMIN, RolsEnum.MODERATOR].some(role => user.roles.include(role));
+        const isOwner = post.authorId === author.id;
+        const hasPrivileges = [RolsEnum.ADMIN, RolsEnum.MODERATOR].some(role => author.roles.include(role));
 
         if (!isOwner && !hasPrivileges) throw new ForbiddenException( POST_ERRORS.FORBIDDEN(CrudEnums.DELETE) );
         
